1f8542b4f19f64a7bd917bd516fcd7aa10cf0dc0
mrchhaya <mohitchhaya24@gmail.com>
2020-09-12 13:38:06 -0400

    Update Bot.py to use Alpha_2

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import qwikidata
 import hashlib
 from dotenv import load_dotenv
 import os,random
+import pycountry
 
 load_dotenv('secrets.env')
 discordToken = os.getenv('DISCORD_TOKEN')
@@ -46,11 +47,35 @@ SELECT ?instance_of ?instance_ofLabel ?shares_border_with ?shares_border_withLab
 }
 """
 borderingCountries = return_sparql_query_results(sq2)
+sq3 = """
+SELECT ?sovereign_state ?sovereign_stateLabel ?short_name WHERE {
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+  ?sovereign_state wdt:P31 wd:Q3624078.
+  OPTIONAL { ?sovereign_state wdt:P1813 ?short_name. }
+}
+"""
+shortNames = return_sparql_query_results(sq3)
 
+def shortHand(s):
+    list_alpha_2 = [i.alpha_2 for i in list(pycountry.countries)]
+    list_alpha_3 = [i.alpha_3 for i in list(pycountry.countries)]
+    for i in range(len(shortNames.get('results').get('bindings'))):
+        try:
+            if s == shortNames.get('results').get('bindings')[i].get('short_name').get('value').lower():
+                return shortNames.get('results').get('bindings')[i].get('sovereign_stateLabel').get('value').lower()
+        except:
+            # return "test"
+            if(len(s)==2 and s.upper() in list_alpha_2):
+                return pycountry.countries.get(alpha_2=s).name.lower()
+            elif(len(s)==3 and s.upper() in list_alpha_3):
+                return pycountry.countries.get(alpha_3=s).name.lower()
+            else:
+                return "invalid"
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Model UN Nerds"))
 
 @client.event
 async def on_message(message):
@@ -63,7 +88,7 @@ async def on_message(message):
         embedV.add_field(name = "Error",value="Couldn't find that country!")
         try:
             for i in range(len(res.get('results').get('bindings'))):
-                if t[4:len(t)] == res.get('results').get('bindings')[i].get('countryLabel').get('value').lower():
+                if t[4:len(t)] == res.get('results').get('bindings')[i].get('countryLabel').get('value').lower() or shortHand(t[4:len(t)]) == res.get('results').get('bindings')[i].get('countryLabel').get('value').lower():
                     try:
                         name = res.get('results').get('bindings')[i].get('flag_image').get('value')[51:-1].replace("%20", "_") + 'g'
                         for j in range(0,10):
@@ -72,7 +97,6 @@ async def on_message(message):
                         m.update(name.encode('utf-8'))
                         h = str(m.hexdigest())
                         url = "https://upload.wikimedia.org/wikipedia/commons/" + h[0] + "/" + h[0] + h[1] + "/" + name
-                        print(url)
                         embedV = discord.Embed(title=res.get('results').get('bindings')[i].get('countryLabel').get('value'),color=int(hex(random.randint(0,16777215)),16), url=url)
                         embedV.set_image(url=str(url))
                         await message.channel.send(url)
@@ -130,11 +154,12 @@ async def on_message(message):
             embedBC.add_field(name = "Bordering Country " + str(i+1) + ": ",value=l[i],inline = False)
         await message.channel.send(embed = embedBC)
     elif t[0:4] == '!bad':
-        await message.channel.send("Fuck " + str(t[5:len(t)]))
+        await message.channel.send("Screw " + str(t[5:len(t)]))
     elif t[0:3] == ck and t.split(" ")[1].lower() == "help":
         embedHelp = discord.Embed(title="Commands: ", color = int(hex(random.randint(0,16777215)),16))
         embedHelp.add_field(name = "Information about Country: ", value = "Returns information about a certain country. Usage: !un {country_name}")
         embedHelp.add_field(name = "Bordering Countries to Country", value = "Returns all of the bordering countries to a certain country. Usage: !unbc {country_name}")
+        embedHelp.add_field(name = "Author: ", value="Mohit Chhaya - https://github.com/mrchhaya", inline=False)
         await message.channel.send(embed = embedHelp)
 client.run(discordToken)
 
@@ -151,3 +176,9 @@ client.run(discordToken)
 # Area ✅
 # Bordering countries ✅
 # Major Religion
+
+
+
+# https://www.un.org/en/sections/documents/general-assembly-resolutions/index.html
+# Be able to find resolutions where [enter country] was involved from https://www.un.org/securitycouncil/content/resolutions-0 and bring them up.
+# Use keywords to bring up resolutions from https://www.un.org/securitycouncil/content/resolutions-0
